a124382dd85c22ba1b1e3e6310c850f1bcebc17d
Guilherme Ramos <guilherme.hor@gmail.com>
2022-09-09 20:55:12 -0300

    Fixes duplicate output messages (issue #6) (#10)
    * fix(packethandlers.lua): first attempt on fixing the duplicate messages
    
    added a packet check to avoid sending duplicates to the client that were
    
    * chore(packethandler): removing comments and changing the function scope
    
    * fix(packethandler): fixes duplicate issues for #6
    
    packet handler now stores chunks on a buffer to check for duplicates
    
    * chore(packethandlers): removing unnecessary lines of code
    
    purged unnecessary lines of code that i was using to debug
    
    * chore(packethandlers): formatting stuff
    
    * fix(ui): fixes a crash happening when opening colors tab

--- lib/packethandlers.lua
+++ lib/packethandlers.lua
@@ -1,9 +1,47 @@
 local ffi = require("ffi");
+local chat = require("chat");
+
 ffi.cdef[[
     int32_t memcmp(const void* buff1, const void* buff2, size_t count);
 ]];
 
 local packethandlers = {};
+
+-- trying to identify possible dupes
+local last_chunk_buffer;
+local reference_buffer = T{};
+function check_duplicates(e)
+    if ffi.C.memcmp(e.data_raw, e.chunk_data_raw, e.size) == 0 then
+        if #reference_buffer > 2 then
+            reference_buffer[#reference_buffer] = nil
+        end
+
+        if last_chunk_buffer then
+            table.insert(reference_buffer, 1, last_chunk_buffer)
+        end
+
+        last_chunk_buffer = T{};
+        local offset = 0;
+    
+        while (offset < e.chunk_size) do
+            local size = ashita.bits.unpack_be(e.chunk_data_raw, offset, 9, 7) * 4;
+            local chunk_packet = struct.unpack('c' .. size, e.chunk_data, offset + 1);
+            last_chunk_buffer:append(chunk_packet)
+            offset = offset + size;
+        end
+    end
+
+    local packet = struct.unpack('c' .. e.size, e.data, 1)
+    for _, chunk in ipairs(reference_buffer) do
+        for _, bufferEntry in ipairs(chunk) do
+            if packet == bufferEntry then
+                e.blocked = true
+                return true
+            end
+        end
+    end
+    return false
+end
 
 packethandlers.HandleIncoming0x00A = function(e)
     local id = struct.unpack('L', e.data, 0x04 + 1);
@@ -50,6 +88,9 @@ packethandlers.HandleIncoming0x28 = function(e)
 end
 
 packethandlers.HandleIncomingPacket = function(e)
+    
+    if check_duplicates(e) then return end
+
 	if (e.id == 0x00A) then
 		gPacketHandlers.HandleIncoming0x00A(e);
     elseif (e.id == 0x28) then
